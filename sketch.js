25204ff041d018862a448c1563ca4f692125e7b5
Zunaid5969 <74778429+Zunaid5969@users.noreply.github.com>
2021-07-17 23:56:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    bobobject1 = new bob(400,575,40)
+	bobobject2 = new bob(400,575,40)
+	bobobject3 = new bob(400,575,40)
+	bobobject4 = new bob(400,575,40)
+	bobobject5 = new bob(400,575,40)
+	roof=new roof(400,100,550,30);
+
+  rope1=new rope(bobobject1.body,roof.body,110, 0)
+	rope2=new rope(bobobject2.body,roof.body,220, 0)
+	rope3=new rope(bobobject3.body,roof.body,550, 0)
+	rope4=new rope(bobobject4.body,roof.body,330, 0)
+	rope5=new rope(bobobject5.body,roof.body,440, 0)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  bobobject1 .display();
+  bobobject2 .display();
+  bobobject3 .display();
+  bobobject4 .display();
+  bobobject5 .display();
+  rope1.display();
+  rope2.display(); 
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  roof.display();
+  drawSprites();
+  
+  
+}
+
+
+
